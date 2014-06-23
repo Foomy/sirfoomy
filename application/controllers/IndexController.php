fa7dd93442af0b484f4888a19902e71d5a72e721
Sascha Schneider <foomy.code@arcor.de>
2014-06-23 18:35:06 +0200

    Wimip feature added

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -23,4 +23,21 @@ class IndexController extends Zend_Controller_Action
 	public function impressAction()
 	{
 	}
+
+    public function wimipAction()
+    {
+        $blacklist = array(
+            '62.159.31.243'
+        );
+
+        $remoteIp = $_SERVER['REMOTE_ADDR'];
+        $this->view->remoteIp = $remoteIp;
+
+        if (! in_array($remoteIp, $blacklist)) {
+            $file = realpath(APPLICATION_PATH . '/../wimip') . '/addresses.txt';
+            $fh = fopen($file, 'a+');
+            fwrite($fh, "$remoteIp\n");
+            fclose($fh);
+        }
+    }
 }
