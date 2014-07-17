9c3d570cf265613b48fde33e51cff44a464b4c39
Sascha Schneider <foomy.code@arcor.de>
2014-07-17 21:33:23 +0200

    Design changed

--- application/models/Wimip/Table.php
+++ application/models/Wimip/Table.php
@@ -4,23 +4,23 @@ class Model_Wimip_Table extends Model_Table_Abstract
 {
 	const T_NAME = 'wimip';
 
-	const F_IPV4        = 'ip_v4';
-	const F_IPV6        = 'ip_v6';
-	const F_LAST_SEEN   = 'last_seen';
-	const F_USERAGENT   = 'useragent';
+	const F_IPV4      = 'ip_v4';
+	const F_IPV6      = 'ip_v6';
+	const F_LAST_SEEN = 'last_seen';
+	const F_USERAGENT = 'useragent';
 
-	protected $_name        = self::T_NAME;
-	protected $_primary     = self::F_ID;
-	protected $_rowClass    = 'Model_Wimip_Table_Row';
-	protected $_sequence    = true;
+	protected $_name     = self::T_NAME;
+	protected $_primary  = self::F_ID;
+	protected $_rowClass = 'Model_Wimip_Table_Row';
+	protected $_sequence = true;
 
 	private static $_instance = null;
 
 	/**
 	 * Singelton Pattern: Returns an instance of this class.
 	 *
-	 * @param	void
-	 * @return	Model_Magazine_Table
+	 * @param    void
+	 * @return   Model_Wimip_Table_Row
 	 */
 	public static function getInstance()
 	{
@@ -34,8 +34,8 @@ class Model_Wimip_Table extends Model_Table_Abstract
 	/**
 	 * Returns a single row identified by it's ID.
 	 *
-	 * @param	int $id
-	 * @return	Model_Wimip_Table_Row
+	 * @param    int $id
+	 * @return   Model_Wimip_Table_Row
 	 */
 	public function findById($id, $includeInactive = false)
 	{
@@ -45,19 +45,19 @@ class Model_Wimip_Table extends Model_Table_Abstract
 		return $this->fetchRow($select);
 	}
 
-    /**
-     * Returns a single row identified by the IPv4 address.
-     *
-     * @param  int $id
-     * @return Model_Wimip_Table_Row
-     */
-    public function findByIpv4($ipv4)
-    {
-        $select = $this->select();
-        $select->where($this->quote(self::F_IPV4) . '=?', $ipv4);
+	/**
+	 * Returns a single row identified by the IPv4 address.
+	 *
+	 * @param  int $id
+	 * @return Model_Wimip_Table_Row
+	 */
+	public function findByIpv4($ipv4)
+	{
+		$select = $this->select();
+		$select->where($this->quote(self::F_IPV4) . '=?', $ipv4);
 
-        return $this->fetchRow($select);
-    }
+		return $this->fetchRow($select);
+	}
 
 	/**
 	 * Checks, whether a IP address is already stored.
@@ -67,26 +67,27 @@ class Model_Wimip_Table extends Model_Table_Abstract
 	 */
 	public function ipv4Exists($ipv4)
 	{
-		$sql = "SELECT 1 FROM `%s` WHERE `%s` = '%s'";
-        $binded = sprintf($sql, self::T_NAME, self::F_IPV4, $ipv4);
+		$sql    = "SELECT 1 FROM `%s` WHERE `%s` = '%s'";
+		$binded = sprintf($sql, self::T_NAME, self::F_IPV4, $ipv4);
+
 		return (bool)$this->getAdapter()->fetchOne($binded);
 	}
 
 	/**
 	 * Returns a rowset with all entries stored in the table.
 	 *
-	 * @param	int $limit		Optional! Default: 0
-	 * @param	int $offset		Optional! Default: 0
-	 * @param	array $order	Optional! Default: ID-Field ascending
-	 * @return	Zend_Db_Table_Rowset_Abstract
+	 * @param    int   $limit        Optional! Default: 0
+	 * @param    int   $offset        Optional! Default: 0
+	 * @param    array $order    Optional! Default: ID-Field ascending
+	 * @return    Zend_Db_Table_Rowset_Abstract
 	 */
 	public function getAll($limit = 0, $offset = 0, array $order = array())
 	{
-		if ((! isset($order['field'])) || empty($order['field'])) {
+		if ((!isset($order['field'])) || empty($order['field'])) {
 			$order['field'] = self::F_ID;
 		}
 
-		if ((! isset($order['direction'])) || empty($order['direction'])) {
+		if ((!isset($order['direction'])) || empty($order['direction'])) {
 			$order['direction'] = self::ASC;
 		}
 
@@ -100,18 +101,18 @@ class Model_Wimip_Table extends Model_Table_Abstract
 	/**
 	 * Returns a rowset with all entries stored in the table.
 	 *
-	 * @param	int $limit		Optional! Default: 0
-	 * @param	int $offset		Optional! Default: 0
-	 * @param	array $order	Optional! Default: ID-Field ascending
-	 * @return	Zend_Paginator
+	 * @param    int   $limit        Optional! Default: 0
+	 * @param    int   $offset        Optional! Default: 0
+	 * @param    array $order    Optional! Default: ID-Field ascending
+	 * @return    Zend_Paginator
 	 */
 	public function getAllPaged($pageNumber, $itemCountPerPage, array $order = array())
 	{
-		if ((! isset($order['field'])) || empty($order['field'])) {
+		if ((!isset($order['field'])) || empty($order['field'])) {
 			$order['field'] = self::F_ID;
 		}
 
-		if ((! isset($order['direction'])) || empty($order['direction'])) {
+		if ((!isset($order['direction'])) || empty($order['direction'])) {
 			$order['direction'] = self::ASC;
 		}
 
@@ -124,5 +125,4 @@ class Model_Wimip_Table extends Model_Table_Abstract
 
 		return $paginator;
 	}
-
 }
