fdc8583717c4f3e592fb79cb80de3ab023886f0e
Sascha Schneider <foomy.code@arcor.de>
2014-07-23 19:35:38 +0200

    New page for prime number gimmickry.

---
+++ application/controllers/PrimesController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: SirFoomy
+ * Date: 18.07.14
+ * Time: 07:31
+ * To change this template use File | Settings | File Templates.
+ */
+
+class PrimesController extends Foo_Controller_Abstract
+{
+	public function indexAction()
+	{
+		$this->view->layout = 'oneCol';
+
+		$primes = null;
+		$form   = new Form_Primenumber();
+
+		if ($this->getRequest()->isPost()) {
+			if ($form->isValid($this->getRequest()->getPost())) {
+				$number = $form->getValue('number');
+				$primes = new Foo_Math_Primes($number);
+			}
+		}
+
+		$this->view->form   = $form;
+		$this->view->primes = $primes;
+	}
+}
