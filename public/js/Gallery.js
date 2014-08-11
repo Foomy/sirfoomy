53046f371ff9a6d89806a7d2b70c1671b00b8161
Sascha Schneider <foomy.code@arcor.de>
2014-08-11 07:47:50 +0200

    Gallery album implemented

--- public/js/Gallery.js
+++ public/js/Gallery.js
@@ -4,6 +4,7 @@
 
 	init: function () {
 		this.bindAlbumCover();
+		this.bindThumbnails();
 	},
 
 	bindAlbumCover: function () {
@@ -19,6 +20,20 @@
 
 			$('.is_rotLeft'+galId).css('left', '0px');
 			$('.is_rotRight'+galId).css('left', '0px');
+		});
+	},
+
+	bindThumbnails: function () {
+		var fadingSpeed = 700;
+
+		$('.is_tNail').unbind('click').bind('click.showImage', function () {
+			var imageId = $(this).data('id');
+			var imgSrc = $('.is_tnMeta'+imageId).data('imgsrc');
+//console.log(imgSrc);
+			$('.is_mainview').fadeOut(fadingSpeed, function () {
+				$('.is_mainview img').attr('src', imgSrc);
+			});
+			$('.is_mainview').fadeIn(fadingSpeed);
 		});
 	}
 };
