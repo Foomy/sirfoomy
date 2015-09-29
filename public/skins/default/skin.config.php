7cfb4bf4b492aae45183146c6a9b5441b0bd7010
Sascha Schneider <foomy.code@arcor.de>
2015-09-29 12:42:12 +0200

    Work templates for main layout and impress added.

---
+++ public/skins/default/skin.config.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+  'stylesheets' => array(
+    'bootstrap-3.3.5/bootstrap.css',
+    'bootstrap-3.3.5/bootstrap-theme.css',
+    'common.css',
+    'layout.css',
+    'main.css'
+  )
+);
+
+
+/**
+ * "Please let me know if there is some other way we can screw up tonight."
+ * (James T. Kirk, Star Trek VI - The Undiscovered Country)
+ */
---
+++ public/templates/index.php
@@ -0,0 +1,17 @@
+<?php require_once 'html-header.phtml'; ?>
+<div id="work-templates-index">
+  <h1>sirfoomy.de - work templates</h1>
+
+  <h3>Layout</h3>
+  <ul>
+    <li><a href="layout-main.phtml" target="_blank">Mainlayout</a></li>
+  </ul>
+
+  <h3>Law pages</h3>
+  <ul>
+    <li><a href="impress.phtml">Impressum</a></li>
+    <li><a href="contact.phtml">Kontakt</a></li>
+    <li><a href="privacy.phtml">Datenschutz</a></li>
+  </ul>
+</div>
+<?php require_once 'html-footer.phtml'; ?>
