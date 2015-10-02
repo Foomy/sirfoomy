075f1e41f499fe395501211cb3f02a541a452219
Sascha Schneider <foomy.code@arcor.de>
2015-10-02 10:44:16 +0200

    Link targets _blank added.

--- public/templates/index.php
+++ public/templates/index.php
@@ -7,11 +7,17 @@
     <li><a href="layout-main.phtml" target="_blank">Mainlayout</a></li>
   </ul>
 
+  <h3>Content pages</h3>
+  <ul>
+    <li><a href="home.phtml" target="_blank">Startseite</a></li>
+    <li><a href="curriculum-vitae.phtml" target="_blank">Lebenslauf</a></li>
+  </ul>
+
   <h3>Law pages</h3>
   <ul>
-    <li><a href="impress.phtml">Impressum</a></li>
-    <li><a href="contact.phtml">Kontakt</a></li>
-    <li><a href="privacy.phtml">Datenschutz</a></li>
+    <li><a href="impress.phtml" target="_blank">Impressum</a></li>
+    <li><a href="contact.phtml" target="_blank">Kontakt</a></li>
+    <li><a href="privacy.phtml" target="_blank">Datenschutz</a></li>
   </ul>
 </div>
 <?php require_once 'html-footer.phtml'; ?>
