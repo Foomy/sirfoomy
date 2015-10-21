878a14c1734596bb1bf6ead40d2878bc7dcd48bc
Sascha Schneider <foomy.code@arcor.de>
2015-10-21 08:53:10 +0200

    Constant for application environment set.

--- public/index.php
+++ public/index.php
@@ -5,13 +5,17 @@
  */
 chdir(dirname(__DIR__));
 
+// Ensure environment constant is set.
+defined('APPLICATION_ENV')
+  || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+
 // Decline static file requests back to the PHP built-in webserver
-if (php_sapi_name() === 'cli-server') {
-    $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-    if (__FILE__ !== $path && is_file($path)) {
-        return false;
-    }
-    unset($path);
+if ( php_sapi_name() === 'cli-server' ) {
+  $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+  if ( __FILE__ !== $path && is_file($path) ) {
+    return false;
+  }
+  unset($path);
 }
 
 // Setup autoloading
